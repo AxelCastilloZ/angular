6f68431932a5234701d011e84ffc8dd90a6981c7
Axel <axel.j.zuniga@gmail.com>
2025-02-10 01:20:27 -0600

    goodService

--- ingresos/src/app/paginas/ingresos/ingresos.component.ts
+++ ingresos/src/app/paginas/ingresos/ingresos.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { FrmIngresosComponent } from '../../componentes/ingresos/frm-ingresos/frm-ingresos.component';
+import { FrmIngresosComponent } from '../../componentes/ingresos/frm-ingresos/frm-ingresos.component'; // Importa FrmIngresosComponent
 
 @Component({
   selector: 'app-ingresos',
+  standalone: true,  // Asegúrate de indicar que el componente es standalone
   templateUrl: './ingresos.component.html',
-  styleUrls: ['./ingresos.component.css']
+  styleUrls: ['./ingresos.component.css'],
+  imports: [FrmIngresosComponent]  // Agrega FrmIngresosComponent en los imports
 })
-export class IngresosComponent {}
+export class IngresosComponent {
+  // Aquí puedes agregar lógica si es necesario
+}
